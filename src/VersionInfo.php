80b989847011de47b493eb2c46e8a7d5e2d8efef
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-06-17 14:43:29 +0700

    Update VersionInfo.php

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -30,7 +30,7 @@ use function is_int;
 use function str_repeat;
 
 final class VersionInfo{
-	public const NAME = "PocketMine-MP";
+	public const NAME = "XPocket-MP";
 	public const BASE_VERSION = "5.16.1";
 	public const IS_DEVELOPMENT_BUILD = true;
 	public const BUILD_CHANNEL = "stable";
