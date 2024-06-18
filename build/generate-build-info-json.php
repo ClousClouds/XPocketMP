ddbaa9978a5bc3efb04743114c10dafda023ff61
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-06-18 15:50:08 +0700

    Update generate-build-info-json.php

--- build/generate-build-info-json.php
+++ build/generate-build-info-json.php
@@ -41,7 +41,7 @@ echo json_encode([
 	"mcpe_version" => ProtocolInfo::MINECRAFT_VERSION_NETWORK,
 	"date" => time(), //TODO: maybe we should embed this in VersionInfo?
 	"details_url" => "https://github.com/$argv[3]/releases/tag/$argv[2]",
-	"download_url" => "https://github.com/$argv[3]/releases/download/$argv[2]/PocketMine-MP.phar",
+	"download_url" => "https://github.com/$argv[3]/releases/download/$argv[2]/XPocketMP.phar",
 	"source_url" => "https://github.com/$argv[3]/tree/$argv[2]",
 	"build_log_url" => "https://github.com/$argv[3]/actions/runs/$argv[5]",
 	"php_download_url" => $argv[6],
