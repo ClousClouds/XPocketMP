1c9ec69ddd16a49609246fa6757acf1225637f68
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-06-22 13:01:44 +0700

    Added so that villagers can walk in Village.php

--- src/entity/Villager.php
+++ src/entity/Villager.php
@@ -2,11 +2,12 @@
 
 /*
  *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *  __   _______           _        _   __  __  _____      __  __ _____
+ *  \ \ / /  __ \         | |      | | |  \/  |/ ____|    |  \/  |  __ \
+ *   \ V /| |__) |__   ___| | _____| |_| \  / | |   ______| \  / | |__) |
+ *    > < |  ___/ _ \ / __| |/ / _ \ __| |\/| | |  |______| |\/| |  ___/
+ *   / . \| |  | (_) | (__|   <  __/ |_| |  | | |____     | |  | | |
+ *  /_/ \_\_|   \___/ \___|_|\_\___|\__|_|  |_|\_____|    |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -15,7 +16,6 @@
  *
  * @author PocketMine Team
  * @link http://www.pocketmine.net/
- *
  *
  */
 
@@ -28,69 +28,162 @@ use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataCollection;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataFlags;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
+use pocketmine\world\World;
+use pocketmine\block\BlockLegacyIds;
+use pocketmine\block\Crops;
+use pocketmine\block\Farmland;
+use pocketmine\math\Vector3;
 
-class Villager extends Living implements Ageable{
-	public const PROFESSION_FARMER = 0;
-	public const PROFESSION_LIBRARIAN = 1;
-	public const PROFESSION_PRIEST = 2;
-	public const PROFESSION_BLACKSMITH = 3;
-	public const PROFESSION_BUTCHER = 4;
+class Villager extends Living implements Ageable {
+    public const PROFESSION_FARMER = 0;
+    public const PROFESSION_LIBRARIAN = 1;
+    public const PROFESSION_PRIEST = 2;
+    public const PROFESSION_BLACKSMITH = 3;
+    public const PROFESSION_BUTCHER = 4;
 
-	private const TAG_PROFESSION = "Profession"; //TAG_Int
+    private const TAG_PROFESSION = "Profession"; // TAG_Int
 
-	public static function getNetworkTypeId() : string{ return EntityIds::VILLAGER; }
+    private bool $baby = false;
+    private int $profession = self::PROFESSION_FARMER;
 
-	private bool $baby = false;
-	private int $profession = self::PROFESSION_FARMER;
+    private array $villageBoundaries = [ // Defining the village boundaries
+        'x_min' => 0,
+        'x_max' => 100,
+        'z_min' => 0,
+        'z_max' => 100,
+    ];
 
-	protected function getInitialSizeInfo() : EntitySizeInfo{
-		return new EntitySizeInfo(1.8, 0.6); //TODO: eye height??
-	}
+    private array $farmPlots = [];
 
-	public function getName() : string{
-		return "Villager";
-	}
+    public static function getNetworkTypeId() : string {
+        return EntityIds::VILLAGER;
+    }
 
-	protected function initEntity(CompoundTag $nbt) : void{
-		parent::initEntity($nbt);
+    protected function getInitialSizeInfo() : EntitySizeInfo {
+        return new EntitySizeInfo(1.8, 0.6); // TODO: eye height??
+    }
 
-		/** @var int $profession */
-		$profession = $nbt->getInt(self::TAG_PROFESSION, self::PROFESSION_FARMER);
+    public function getName() : string {
+        return "Villager";
+    }
 
-		if($profession > 4 || $profession < 0){
-			$profession = self::PROFESSION_FARMER;
-		}
+    protected function initEntity(CompoundTag $nbt) : void {
+        parent::initEntity($nbt);
 
-		$this->setProfession($profession);
-	}
+        /** @var int $profession */
+        $profession = $nbt->getInt(self::TAG_PROFESSION, self::PROFESSION_FARMER);
 
-	public function saveNBT() : CompoundTag{
-		$nbt = parent::saveNBT();
-		$nbt->setInt(self::TAG_PROFESSION, $this->getProfession());
+        if ($profession > 4 || $profession < 0) {
+            $profession = self::PROFESSION_FARMER;
+        }
 
-		return $nbt;
-	}
+        $this->setProfession($profession);
 
-	/**
-	 * Sets the villager profession
-	 */
-	public function setProfession(int $profession) : void{
-		$this->profession = $profession; //TODO: validation
-		$this->networkPropertiesDirty = true;
-	}
+        // Generate random farm plots
+        $this->generateRandomFarmPlots();
+    }
 
-	public function getProfession() : int{
-		return $this->profession;
-	}
+    public function saveNBT() : CompoundTag {
+        $nbt = parent::saveNBT();
+        $nbt->setInt(self::TAG_PROFESSION, $this->getProfession());
 
-	public function isBaby() : bool{
-		return $this->baby;
-	}
+        return $nbt;
+    }
 
-	protected function syncNetworkData(EntityMetadataCollection $properties) : void{
-		parent::syncNetworkData($properties);
-		$properties->setGenericFlag(EntityMetadataFlags::BABY, $this->baby);
+    /**
+     * Sets the villager profession
+     */
+    public function setProfession(int $profession) : void {
+        $this->profession = $profession; // TODO: validation
+        $this->networkPropertiesDirty = true;
+    }
 
-		$properties->setInt(EntityMetadataProperties::VARIANT, $this->profession);
-	}
+    public function getProfession() : int {
+        return $this->profession;
+    }
+
+    public function isBaby() : bool {
+        return $this->baby;
+    }
+
+    protected function syncNetworkData(EntityMetadataCollection $properties) : void {
+        parent::syncNetworkData($properties);
+        $properties->setGenericFlag(EntityMetadataFlags::BABY, $this->baby);
+        $properties->setInt(EntityMetadataProperties::VARIANT, $this->profession);
+    }
+
+    public function onUpdate(int $currentTick) : bool {
+        parent::onUpdate($currentTick);
+
+        // Add walking logic
+        $this->wanderAround();
+
+        // Add farming logic
+        if ($this->profession === self::PROFESSION_FARMER) {
+            $this->farmCrops();
+        }
+
+        return true;
+    }
+
+    private function wanderAround() : void {
+        // Simple wandering logic within the village boundaries
+        if (mt_rand(0, 100) < 5) { // 5% chance to move every tick
+            $newX = mt_rand($this->villageBoundaries['x_min'], $this->villageBoundaries['x_max']);
+            $newZ = mt_rand($this->villageBoundaries['z_min'], $this->villageBoundaries['z_max']);
+            $this->moveTo($newX, $this->getY(), $newZ);
+        }
+    }
+
+    private function farmCrops() : void {
+        // Logic to find a farm plot and perform farming actions
+        foreach ($this->farmPlots as $plot) {
+            if ($this->distanceSquared($plot['x'], $plot['y'], $plot['z']) < 2) {
+                // Simulate farming by planting and harvesting crops
+                $this->harvestAndReplant($plot['x'], $plot['y'], $plot['z']);
+            } else {
+                // Move towards the farm plot
+                $this->moveTo($plot['x'], $plot['y'], $plot['z']);
+            }
+        }
+    }
+
+    private function harvestAndReplant(int $x, int $y, int $z) : void {
+        $world = $this->getWorld();
+        $block = $world->getBlockAt($x, $y, $z);
+
+        // Check if the block is a crop and fully grown
+        if ($block instanceof Crops && $block->getAge() === Crops::MAX_AGE) {
+            // Harvest the crop
+            $world->useBreakOn($block);
+
+            // Replant the crop
+            $world->setBlockAt($x, $y, $z, BlockLegacyIds::WHEAT_BLOCK);
+        }
+    }
+
+    private function distanceSquared(int $x, int $y, int $z) : float {
+        return ($this->getX() - $x) ** 2 + ($this->getY() - $y) ** 2 + ($this->getZ() - $z) ** 2;
+    }
+
+    private function moveTo(int $x, int $y, int $z) : void {
+        // Logic to move villager to the specified coordinates
+        $this->setPosition($x, $y, $z);
+    }
+
+    private function setPosition(int $x, int $y, int $z) : void {
+        // Implement actual position setting logic
+        $this->teleport(new Vector3($x, $y, $z));
+    }
+
+    private function generateRandomFarmPlots() : void {
+        // Generate a set of random farm plots within the village boundaries
+        $numPlots = mt_rand(1, 5); // Number of farm plots to generate
+        for ($i = 0; $i < $numPlots; $i++) {
+            $x = mt_rand($this->villageBoundaries['x_min'], $this->villageBoundaries['x_max']);
+            $z = mt_rand($this->villageBoundaries['z_min'], $this->villageBoundaries['z_max']);
+            $y = $this->getWorld()->getHighestBlockAt($x, $z)->getY(); // Get the highest Y coordinate at (x, z)
+            $this->farmPlots[] = ['x' => $x, 'y' => $y, 'z' => $z];
+        }
+    }
 }
