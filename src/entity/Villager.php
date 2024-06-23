28543c3da64cb4046b5ac03cd9a2292a130643bd
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-06-23 21:06:43 +0700

    Update Villager.php

--- src/entity/Villager.php
+++ src/entity/Villager.php
@@ -18,7 +18,6 @@
  * @link http://www.pocketmine.net/
  *
  */
-
 declare(strict_types=1);
 
 namespace pocketmine\entity;
@@ -28,150 +27,69 @@ use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataCollection;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataFlags;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
-use pocketmine\world\World;
-use pocketmine\block\Block;
-use pocketmine\block\Crops;
-use pocketmine\block\Farmland;
-use pocketmine\math\Vector3;
-use pocketmine\block\BlockToolType;
-use pocketmine\block\BlockIdentifier;
+use xpocketmc\xbvy\VBT;
+class Villager extends Living implements Ageable{
+	public const PROFESSION_FARMER = 0;
+	public const PROFESSION_LIBRARIAN = 1;
+	public const PROFESSION_PRIEST = 2;
+	public const PROFESSION_BLACKSMITH = 3;
+	public const PROFESSION_BUTCHER = 4;
 
-class Villager extends Living implements Ageable {
-    public const PROFESSION_FARMER = 0;
-    public const PROFESSION_LIBRARIAN = 1;
-    public const PROFESSION_PRIEST = 2;
-    public const PROFESSION_BLACKSMITH = 3;
-    public const PROFESSION_BUTCHER = 4;
+	private const TAG_PROFESSION = "Profession"; //TAG_Int
 
-    private const TAG_PROFESSION = "Profession"; // TAG_Int
+	public static function getNetworkTypeId() : string{ return EntityIds::VILLAGER; }
 
-    private bool $baby = false;
-    private int $profession = self::PROFESSION_FARMER;
+	private bool $baby = false;
+	private int $profession = self::PROFESSION_FARMER;
 
-    /** @var array<string, int> */
-    private array $villageBoundaries = [
-        'x_min' => 0,
-        'x_max' => 100,
-        'z_min' => 0,
-        'z_max' => 100,
-    ];
+	protected function getInitialSizeInfo() : EntitySizeInfo{
+		return new EntitySizeInfo(1.8, 0.6); //TODO: eye height??
+	}
 
-    /** @var array<array<string, int>> */
-    private array $farmPlots = [];
+	public function getName() : string{
+		return "Villager";
+	}
 
-    public static function getNetworkTypeId() : string {
-        return EntityIds::VILLAGER;
-    }
+	protected function initEntity(CompoundTag $nbt) : void{
+		parent::initEntity($nbt);
 
-    protected function getInitialSizeInfo() : EntitySizeInfo {
-        return new EntitySizeInfo(1.8, 0.6);
-    }
+		/** @var int $profession */
+		$profession = $nbt->getInt(self::TAG_PROFESSION, self::PROFESSION_FARMER);
 
-    public function getName() : string {
-        return "Villager";
-    }
+		if($profession > 4 || $profession < 0){
+			$profession = self::PROFESSION_FARMER;
+		}
 
-    protected function initEntity(CompoundTag $nbt) : void {
-        parent::initEntity($nbt);
+		$this->setProfession($profession);
+	}
 
-        /** @var int $profession */
-        $profession = $nbt->getInt(self::TAG_PROFESSION, self::PROFESSION_FARMER);
+	public function saveNBT() : CompoundTag{
+		$nbt = parent::saveNBT();
+		$nbt->setInt(self::TAG_PROFESSION, $this->getProfession());
 
-        if ($profession > 4 || $profession < 0) {
-            $profession = self::PROFESSION_FARMER;
-        }
+		return $nbt;
+	}
 
-        $this->setProfession($profession);
+	/**
+	 * Sets the villager profession
+	 */
+	public function setProfession(int $profession) : void{
+		$this->profession = $profession; //TODO: validation
+		$this->networkPropertiesDirty = true;
+	}
 
-        // Generate random farm plots
-        $this->generateRandomFarmPlots();
-    }
+	public function getProfession() : int{
+		return $this->profession;
+	}
 
-    public function saveNBT() : CompoundTag {
-        $nbt = parent::saveNBT();
-        $nbt->setInt(self::TAG_PROFESSION, $this->getProfession());
+	public function isBaby() : bool{
+		return $this->baby;
+	}
 
-        return $nbt;
-    }
+	protected function syncNetworkData(EntityMetadataCollection $properties) : void{
+		parent::syncNetworkData($properties);
+		$properties->setGenericFlag(EntityMetadataFlags::BABY, $this->baby);
 
-    public function setProfession(int $profession) : void {
-        $this->profession = $profession;
-        $this->networkPropertiesDirty = true;
-    }
-
-    public function getProfession() : int {
-        return $this->profession;
-    }
-
-    public function isBaby() : bool {
-        return $this->baby;
-    }
-
-    protected function syncNetworkData(EntityMetadataCollection $properties) : void {
-        parent::syncNetworkData($properties);
-        $properties->setGenericFlag(EntityMetadataFlags::BABY, $this->baby);
-        $properties->setInt(EntityMetadataProperties::VARIANT, $this->profession);
-    }
-
-    public function onUpdate(int $currentTick) : bool {
-        parent::onUpdate($currentTick);
-
-        $this->wanderAround();
-
-        if ($this->profession === self::PROFESSION_FARMER) {
-            $this->farmCrops();
-        }
-
-        return true;
-    }
-
-    private function wanderAround() : void {
-        if (mt_rand(0, 100) < 5) {
-            $newX = mt_rand($this->villageBoundaries['x_min'], $this->villageBoundaries['x_max']);
-            $newZ = mt_rand($this->villageBoundaries['z_min'], $this->villageBoundaries['z_max']);
-            $this->moveTo($newX, (int) $this->getPosition()->getY(), $newZ);
-        }
-    }
-
-    private function farmCrops() : void {
-        foreach ($this->farmPlots as $plot) {
-            if ($this->distanceSquared($plot['x'], $plot['y'], $plot['z']) < 2) {
-                $this->harvestAndReplant($plot['x'], $plot['y'], $plot['z']);
-            } else {
-                $this->moveTo($plot['x'], $plot['y'], $plot['z']);
-            }
-        }
-    }
-
-    private function harvestAndReplant(int $x, int $y, int $z) : void {
-        $world = $this->getWorld();
-        $block = $world->getBlockAt($x, $y, $z);
-
-        if ($block instanceof Crops && $block->getAge() === Crops::MAX_AGE) {
-            $world->useBreakOn(new Vector3($x, $y, $z));
-
-            $blockFactory = BlockFactory::getInstance();
-            $wheatBlock = $blockFactory->get(Block::WHEAT_BLOCK);
-            $world->setBlockAt($x, $y, $z, $wheatBlock);
-        }
-    }
-
-    private function distanceSquared(int $x, int $y, int $z) : float {
-        $pos = $this->getPosition();
-        return ($pos->getX() - $x) ** 2 + ($pos->getY() - $y) ** 2 + ($pos->getZ() - $z) ** 2;
-    }
-
-    private function moveTo(int $x, int $y, int $z) : void {
-        $this->setPosition(new Vector3($x, $y, $z));
-    }
-
-    private function generateRandomFarmPlots() : void {
-        $numPlots = mt_rand(1, 5);
-        for ($i = 0; $i < $numPlots; $i++) {
-            $x = mt_rand($this->villageBoundaries['x_min'], $this->villageBoundaries['x_max']);
-            $z = mt_rand($this->villageBoundaries['z_min'], $this->villageBoundaries['z_max']);
-            $y = $this->getWorld()->getHighestBlockAt($x, $z)->getPosition()->getY();
-            $this->farmPlots[] = ['x' => $x, 'y' => $y, 'z' => $z];
-        }
-    }
+		$properties->setInt(EntityMetadataProperties::VARIANT, $this->profession);
+	}
 }
