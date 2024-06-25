341aa2c086a1d5a6dd2121f8cf4871e08dad6223
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-06-25 16:31:14 +0700

    Update Villager.php

--- src/entity/Villager.php
+++ src/entity/Villager.php
@@ -2,19 +2,18 @@
 
 /*
  *
- *  __   _______           _        _   __  __  _____      __  __ _____
- *  \ \ / /  __ \         | |      | | |  \/  |/ ____|    |  \/  |  __ \
- *   \ V /| |__) |__   ___| | _____| |_| \  / | |   ______| \  / | |__) |
- *    > < |  ___/ _ \ / __| |/ / _ \ __| |\/| | |  |______| |\/| |  ___/
- *   / . \| |  | (_) | (__|   <  __/ |_| |  | | |____     | |  | | |
- *  /_/ \_\_|   \___/ \___|_|\_\___|\__|_|  |_|\_____|    |_|  |_|_|
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @aut>...
+ * @author PocketMine Team
  * @link http://www.pocketmine.net/
  *
  */
