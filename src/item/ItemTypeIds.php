d9d8f46a57425e1d354a92d1324c87b3ddf40420
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-06-25 20:19:42 +0700

    Update ItemTypeIds.php

--- src/item/ItemTypeIds.php
+++ src/item/ItemTypeIds.php
@@ -326,7 +326,7 @@ final class ItemTypeIds{
 	public const NAME_TAG = 20287;
 
 	public const FIRST_UNUSED_ITEM_ID = 20288;
-	public const COW_SPAWN_EGG = 202889;
+	public const COW_SPAWN_EGG = 20289;
 
 	private static int $nextDynamicId = self::FIRST_UNUSED_ITEM_ID;
 
