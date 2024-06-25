c578fc1ac97d987d700f27298b99728b78d351c4
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-06-25 17:51:27 +0700

    Update Cow.php

--- src/entity/Cow.php
+++ src/entity/Cow.php
@@ -24,7 +24,7 @@ declare(strict_types=1);
 namespace pocketmine\entity;
 
 use pocketmine\item\Item;
-use pocketmine\level\Level;
+use pocketmine\entity\Location;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\protocol\AddActorPacket;
 use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
@@ -32,37 +32,47 @@ use function mt_rand;
 
 class Cow extends Living
 {
-          public const NETWORK_ID = EntityIds::COW;
+    public const NETWORK_ID = EntityIds::COW;
 
-          public function __construct(Level $level, CompoundTag $nbt)
-          {
-                          parent::__construct($level, $nbt);
-          }
+    public function __construct(Location $location, CompoundTag $nbt)
+    {
+        parent::__construct($location, $nbt);
+    }
 
-          public function getName() : string
-          {
-                          return "Cow";
-          }
+    public function getName() : string
+    {
+        return "Cow";
+    }
 
-          public function getDrops() : array
-          {
-                          return [
-                                  Item::get(Item::RAW_BEEF, 0, mt_rand(1, 3)), // Drop 1-3 raw beef
-                                  Item::get(Item::LEATHER, 0, mt_rand(0, 2))  // Drop 0-2 leather
-                          ];
-          }
+    public function getDrops() : array
+    {
+        return [
+            Item::get(ItemIds::RAW_BEEF, 0, mt_rand(1, 3)), // Drop 1-3 raw beef
+            Item::get(ItemIds::LEATHER, 0, mt_rand(0, 2))  // Drop 0-2 leather
+        ];
+    }
 
-          protected function addSpawnPacket(Player $player) : void
-          {
-                          $pk = new AddActorPacket();
-                          $pk->type = self::NETWORK_ID;
-                          $pk->entityRuntimeId = $this->getId();
-                          $pk->position = $this->asVector3();
-                          $pk->motion = $this->getMotion();
-                          $pk->yaw = $this->yaw;
-                          $pk->pitch = $this->pitch;
-                          $pk->headYaw = $this->yaw;
+    protected function addSpawnPacket(Player $player) : void
+    {
+        $pk = new AddActorPacket();
+        $pk->type = self::NETWORK_ID;
+        $pk->entityRuntimeId = $this->getId();
+        $pk->position = $this->getPosition();
+        $pk->motion = $this->getMotion();
+        $pk->yaw = $this->yaw;
+        $pk->pitch = $this->pitch;
+        $pk->headYaw = $this->yaw;
 
-                          $this->server->broadcastPacket($this->getViewers(), $pk);
-          }
+        $this->server->broadcastPacket($this->getViewers(), $pk);
+    }
+
+    public function getInitialSizeInfo() : EntitySizeInfo
+    {
+        return new EntitySizeInfo(1.4, 0.9); // tinggi 1.4 unit, lebar 0.9 unit
+    }
+
+    public function getNetworkTypeId() : string
+    {
+        return EntityIds::COW;
+    }
 }
