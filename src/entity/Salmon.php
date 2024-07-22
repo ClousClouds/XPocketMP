1b49e4ceafdc7057a5f859ca5d79adcffd4d940a
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-22 21:43:52 +0700

    Update for PHP-CS-FIXER

--- src/entity/Salmon.php
+++ src/entity/Salmon.php
@@ -13,13 +13,10 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 declare(strict_types=1);
