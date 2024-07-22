add280e5836296eef04b34c5cca580970d2c1abb
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-22 21:30:23 +0700

    Adding Licence Headers #2829

--- src/entity/Salmon.php
+++ src/entity/Salmon.php
@@ -1,24 +1,48 @@
 <?php
 
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
 namespace pocketmine\entity;
 
-use pocketmine\item\Item;
-use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\entity\animation\BubbleParticleAnimation;
+use pocketmine\entity\utils\RandomSwimDirection;
+use pocketmine\item\VanillaItems;
 use pocketmine\math\Vector3;
-use pocketmine\level\particle\BubbleParticle;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\world\World;
 
 class Salmon extends WaterAnimal {
-
     public const NETWORK_ID = self::SALMON;
 
-    public $width = 0.7;
-    public $height = 0.4;
+    private float $width = 0.7;
+    private float $height = 0.4;
+    private Vector3 $swimDirection;
+    private int $changeDirectionTicks = 0;
 
-    private $swimDirection;
-    private $changeDirectionTicks = 0;
-
-    public function __construct(Level $level, CompoundTag $nbt) {
-        parent::__construct($level, $nbt);
+    public function __construct(Location $location, ?CompoundTag $nbt = null) {
+        parent::__construct($location, $nbt);
         $this->setMaxHealth(3);
         $this->setHealth($this->getMaxHealth());
         $this->swimDirection = new Vector3(0, 0, 0);
@@ -28,14 +52,22 @@ class Salmon extends WaterAnimal {
         return "Salmon";
     }
 
-    public function initEntity(): void {
-        parent::initEntity();
+    public function getInitialSizeInfo(): EntitySizeInfo {
+        return new EntitySizeInfo(0.4, 0.7);
+    }
+
+    public function getNetworkTypeId(): string {
+        return "minecraft:salmon";
+    }
+
+    public function initEntity(CompoundTag $nbt): void {
+        parent::initEntity($nbt);
         $this->setGenericFlag(self::DATA_FLAG_IMMOBILE, false);
     }
 
     public function getDrops(): array {
         return [
-            Item::get(Item::RAW_SALMON, 0, 1)
+            VanillaItems::RAW_SALMON()
         ];
     }
 
@@ -46,30 +78,22 @@ class Salmon extends WaterAnimal {
     protected function entityBaseTick(int $tickDiff = 1): bool {
         $hasUpdate = parent::entityBaseTick($tickDiff);
 
-        // Change direction every 100 ticks (5 seconds)
         if ($this->changeDirectionTicks-- <= 0) {
             $this->changeSwimDirection();
             $this->changeDirectionTicks = 100;
         }
 
-        // Move in the current swim direction
         $this->move($this->swimDirection->x, $this->swimDirection->y, $this->swimDirection->z);
-        
-        // Play bubble particles while moving
-        $this->level->addParticle(new BubbleParticle($this->add(0, 0.5, 0)));
+        $this->getWorld()->addParticle($this->getLocation()->add(0, 0.5, 0), new BubbleParticleAnimation());
 
         return $hasUpdate;
     }
 
     private function changeSwimDirection(): void {
-        $this->swimDirection = new Vector3(
-            mt_rand(-10, 10) / 10,
-            mt_rand(-5, 5) / 10,
-            mt_rand(-10, 10) / 10
-        );
+        $this->swimDirection = RandomSwimDirection::generate();
     }
 
-    public function updateMovement(): void {
-        parent::updateMovement();
+    public function updateMovement(bool $teleport = false): void {
+        parent::updateMovement($teleport);
     }
 }
