15a7251c2c36c79aba434881833b26831145e14a
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-22 14:17:08 +0700

    Update version to 5.19.0 for minecraft version 1.20.20

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,7 +31,7 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "XPocketMP";
-	public const BASE_VERSION = "5.18.0";
+	public const BASE_VERSION = "5.19.0";
 	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
 
