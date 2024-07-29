b2ada3a767803cc7f61603a8c0f846629b15a950
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-30 04:57:55 +0700

    Update Salmon.php

--- src/entity/Salmon.php
+++ src/entity/Salmon.php
@@ -34,80 +34,80 @@ use pocketmine\world\particle\BubbleParticle;
 
 class Salmon extends WaterAnimal
 {
-    public const NETWORK_ID = EntityIds::SALMON;
-    public const DATA_FLAG_IMMOBILE = 'DATA_FLAG_IMMOBILE';
+	public const NETWORK_ID = EntityIds::SALMON;
+	public const DATA_FLAG_IMMOBILE = 'DATA_FLAG_IMMOBILE';
 
-    private Vector3 $swimDirection;
-    private int $changeDirectionTicks = 0;
+	private Vector3 $swimDirection;
+	private int $changeDirectionTicks = 0;
 
-    public function __construct(Location $location, ?CompoundTag $nbt = null)
-    {
-        parent::__construct($location, $nbt);
-        $this->setMaxHealth(3);
-        $this->setHealth($this->getMaxHealth());
-        $this->swimDirection = new Vector3(0, 0, 0);
-    }
+	public function __construct(Location $location, ?CompoundTag $nbt = null)
+	{
+		parent::__construct($location, $nbt);
+		$this->setMaxHealth(3);
+		$this->setHealth($this->getMaxHealth());
+		$this->swimDirection = new Vector3(0, 0, 0);
+	}
 
-    public function getName(): string
-    {
-        return "Salmon";
-    }
+	public function getName(): string
+	{
+		return "Salmon";
+	}
 
-    public function getInitialSizeInfo(): EntitySizeInfo
-    {
-        return new EntitySizeInfo(0.4, 0.7);
-    }
+	public function getInitialSizeInfo(): EntitySizeInfo
+	{
+		return new EntitySizeInfo(0.4, 0.7);
+	}
 
-    public static function getNetworkTypeId(): string
-    {
-        return "minecraft:salmon";
-    }
+	public static function getNetworkTypeId() : string
+	{
+		return "minecraft:salmon";
+	}
 
-    public function initEntity(CompoundTag $nbt): void
-    {
-        parent::initEntity($nbt);
-        $this->setGenericFlag(self::DATA_FLAG_IMMOBILE, false);
-    }
+	public function initEntity(CompoundTag $nbt) : void
+	{
+		parent::initEntity($nbt);
+		$this->setGenericFlag(self::DATA_FLAG_IMMOBILE, false);
+	}
 
-    public function getDrops(): array
-    {
-        return [
-            VanillaItems::RAW_SALMON()
-        ];
-    }
+	public function getDrops() : array
+	{
+		return [
+			VanillaItems::RAW_SALMON()
+		];
+	}
 
-    public function getSpeed(): float
-    {
-        return 1.2;
-    }
+	public function getSpeed() : float
+	{
+		return 1.2;
+	}
 
-    protected function entityBaseTick(int $tickDiff = 1): bool
-    {
-        $hasUpdate = parent::entityBaseTick($tickDiff);
+	protected function entityBaseTick(int $tickDiff = 1) : bool
+	{
+		$hasUpdate = parent::entityBaseTick($tickDiff);
 
-        if ($this->changeDirectionTicks-- <= 0) {
-            $this->changeSwimDirection();
-            $this->changeDirectionTicks = 100;
-        }
+		if ($this->changeDirectionTicks-- <= 0) {
+			$this->changeSwimDirection();
+			$this->changeDirectionTicks = 100;
+		}
 
-        $this->move($this->swimDirection->x, $this->swimDirection->y, $this->swimDirection->z);
-        $this->getWorld()->addParticle($this->getLocation()->add(0, 0.5, 0), new BubbleParticle());
+		$this->move($this->swimDirection->x, $this->swimDirection->y, $this->swimDirection->z);
+		$this->getWorld()->addParticle($this->getLocation()->add(0, 0.5, 0), new BubbleParticle());
 
-        return $hasUpdate;
-    }
+		return $hasUpdate;
+	}
 
-    private function changeSwimDirection(): void
-    {
-        $this->swimDirection = RandomSwimDirection::generate();
-    }
+	private function changeSwimDirection() : void
+	{
+		$this->swimDirection = RandomSwimDirection::generate();
+	}
 
-    public function updateMovement(bool $teleport = false): void
-    {
-        parent::updateMovement($teleport);
-    }
+	public function updateMovement(bool $teleport = false) : void
+	{
+		parent::updateMovement($teleport);
+	}
 
-    private function setGenericFlag(string $flag, bool $value): void
-    {
-        $this->propertyManager->setGenericFlag($flag, $value);
-    }
+	private function setGenericFlag(string $flag, bool $value) : void
+	{
+		$this->propertyManager->setGenericFlag($flag, $value);
+	}
 }
