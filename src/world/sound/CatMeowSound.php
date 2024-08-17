29dc06b457c0f642af1b507ea801600c0ff6818c
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-08-18 05:53:10 +0700

    Update CatMeowSound.php

--- src/world/sound/CatMeowSound.php
+++ src/world/sound/CatMeowSound.php
@@ -12,16 +12,5 @@ class CatMeowSound extends Sound {
 
     public function __construct(Vector3 $pos){
         parent::__construct("mob.cat.meow", $pos, 1.0, 1.0); // volume 1.0, pitch 1.0
-    }
-
-    public function encode(Player $player): array {
-        $pk = new PlaySoundPacket();
-        $pk->soundName = "mob.cat.meow";
-        $pk->x = $this->x;
-        $pk->y = $this->y;
-        $pk->z = $this->z;
-        $pk->volume = $this->volume;
-        $pk->pitch = $this->pitch;
-        return [$pk];
-    }
+	}
 }
