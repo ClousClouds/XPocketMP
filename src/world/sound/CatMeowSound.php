7f8a4c2728c27436efc48b6776624fa301a5bbe7
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-08-17 15:13:42 +0700

    Update for 5.20.0

--- src/world/sound/CatMeowSound.php
+++ src/world/sound/CatMeowSound.php
@@ -3,10 +3,12 @@
 namespace pocketmine\world\sound;
 
 use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+use pocketmine\network\mcpe\protocol\types\LevelEvent;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
 use pocketmine\player\Player;
 
-class CatMeowSound extends PlaySound {
+class CatMeowSound extends Sound {
 
     public function __construct(Vector3 $pos){
         parent::__construct("mob.cat.meow", $pos, 1.0, 1.0); // volume 1.0, pitch 1.0
