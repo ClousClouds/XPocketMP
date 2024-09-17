46822ae2c971b3aa9e6ac29f8a65e4564a3b225d
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-09-17 20:44:47 +0700

    Fixed

--- src/PocketPlayerUUID.php
+++ src/PocketPlayerUUID.php
@@ -3,19 +3,29 @@
 namespace pocketmine;
 
 use pocketmine\player\Player;
-use pocketmine\utils\Utils;
+use pocketmine\utils\UUID;
 
 class PocketPlayerUUID {
 
-protected Player $pyer;
+    protected Player $pyer;
 
-  public function __construct(Player $pyer) {
-	  $this->pyer = $pyer;
-  }
+    public function __construct(Player $pyer) {
+        $this->pyer = $pyer;
+    }
 
-  public function getPlayerUUID() : bool {
-    $pyer->getPlayer;
-  return true;
-  }
+    public function setPlayer(Player $pyer): void {
+        $this->pyer = $pyer;
+    }
+
+    public function getPlayerUUID(): string {
+        return $this->pyer->getUniqueId()->toString();
+    }
+
+    public function getPlayerName(): string {
+        return $this->pyer->getName();
+    }
+
+    public function isPlayerOnline(): bool {
+        return $this->pyer->isOnline();
+    }
 }
-    
