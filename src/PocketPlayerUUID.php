472617e4dad2b0a74c5beb1094563c87c246a1fd
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-09-17 18:20:23 +0700

    .

---
+++ src/PocketPlayerUUID.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace pocketmine;
+
+use pocketmine\player\Player;
+use pocketmine\utils\Utils;
+
+class PocketPlayerUUID {
+
+private Player $pyer;
+private Utils $utilss;
+
+  public function getPlayerUUID {
+    $this->pyer = $utilss->getPlayer;
+  return false;
+  }
+}
+    
