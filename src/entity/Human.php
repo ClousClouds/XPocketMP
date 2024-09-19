08c04c8025653470ecf394801e48c0b92b14df0d
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-09-19 18:38:25 +0700

    Assemble 1.21.30

--- src/entity/Human.php
+++ src/entity/Human.php
@@ -178,10 +178,10 @@ class Human extends Living implements ProjectileSource, InventoryHolder{
 		}
 	}
 
-	public function emote(string $emoteId) : void{
+	public function emote(string $emoteId, $emoteLengthTicks = 0) : void{
 		NetworkBroadcastUtils::broadcastEntityEvent(
 			$this->getViewers(),
-			fn(EntityEventBroadcaster $broadcaster, array $recipients) => $broadcaster->onEmote($recipients, $this, $emoteId)
+			fn(EntityEventBroadcaster $broadcaster, array $recipients) => $broadcaster->onEmote($recipients, $this, $emoteId, $emoteLengthTicks)
 		);
 	}
 
