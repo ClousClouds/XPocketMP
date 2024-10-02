0aaaa13ec474c21a82ab1e810dd985acba28de47
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-10-02 21:22:17 +0700

    Dump version

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,7 +31,7 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "XPocketMP";
-	public const BASE_VERSION = "5.21.1";
+	public const BASE_VERSION = "5.21.2";
 	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
 
